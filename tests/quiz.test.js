9eb27d9564b1aa605fbd0efc407575e3a5062a4f
Ruchi Singh <ruchisingh.devops@gmail.com>
2024-03-06 21:05:05 +0530

    Add question to lyrics lab

--- tests/quiz.test.js
+++ tests/quiz.test.js
@@ -3,7 +3,7 @@ const { isOnlyOneOptionTrue } = require("../utils");
 
 test("Only one option should be true", () => {
   QUIZ.forEach((quiz) => {
-    quiz.question.forEach((question) => {
+    quiz.questionList.forEach((question) => {
       console.log(question);
       expect(isOnlyOneOptionTrue(question.options)).toBe(true);
     });
@@ -12,14 +12,16 @@ test("Only one option should be true", () => {
 
 test("Options Array should have a length of 4", () => {
   QUIZ.forEach((quiz) => {
-    quiz.question.forEach((question) => {
+    quiz.questionList.forEach((question) => {
+      console.log(question)
       expect(question.options.length).toBe(4);
+
     });
   });
 });
 
 test("Count Length of Questions", () => {
   QUIZ.forEach((quiz) => {
-    console.log(`${quiz.name}: ${quiz.question.length}`);
+    console.log(`${quiz.name}: ${quiz.questionList.length}`);
   });
 });
