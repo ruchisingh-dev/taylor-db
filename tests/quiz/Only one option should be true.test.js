4b51ec2b9d2ac503435a47d2727e235589ad60cd
Ruchi Singh <ruchisingh.devops@gmail.com>
2024-03-26 11:09:28 +0530

    Test Cleared

--- tests/quiz/Only one option should be true.test.js
+++ tests/quiz/Only one option should be true.test.js
@@ -4,7 +4,11 @@ const { isOnlyOneOptionTrue } = require("../../utils");
 test("Only one option should be true", () => {
   QUIZ.forEach((quiz) => {
     quiz.questionList.forEach((question) => {
+      // if (!isOnlyOneOptionTrue(question.options)) {
+      //   console.error(`Error: Only one option should be true but id:'${question.id}'\n${question.options}`)
+      // }
       expect(isOnlyOneOptionTrue(question.options)).toBe(true);
+      
     });
   });
 });
--- tests/quiz/Options array should have a length of 4.test.js
+++ tests/quiz/Options array should have a length of 4.test.js
@@ -4,6 +4,9 @@ const { hasFourOptions } = require("../../utils");
 test("Options Array should have a length of 4", () => {
   QUIZ.forEach((quiz) => {
     quiz.questionList.forEach((question) => {
+      // if(!hasFourOptions(question.options)){
+      //   console.log(`${question.options}`)
+      // }
       expect(hasFourOptions(question.options)).toBe(true);
     });
   });
